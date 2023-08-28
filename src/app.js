70ed13632a9d865b9971fc0fc8d31e68ffd36900
KamilMarcinowski <kamilmarcinowski2006@gmail.com>
2023-08-28 18:32:35 +0200

    commit2

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const codeArea = document.getElementById("codeArea");
 const output = document.getElementById("output");
 const run = document.getElementById("run");
+const lineNumbers = document.getElementById("lineNumber");
 
-const data = "<DOCTYPE html><html><head><meta charset='UTF-8'></head><body><h1>Hello World</h1></body></html>";
+const data = "<DOCTYPE html>\n<html>\n<head\n<meta charset='UTF-8'\n</head>\n<body>\n<h1>Hello World</h1>\n</body>\n</html>";
 
 function Initialize()
 {
     codeArea.value = data
     runCode();
+    UpdateLines();
+}
+
+function UpdateLines()
+{
+    const lines = codeArea.value.split("\n");
+    const lineNumbersHTML = lines.map((_, index) => index + 1).join('\n');
+    lineNumbers.value = lineNumbersHTML;
+    lineNumbers.scrollTop = codeArea.scrollTop;
 }
 
 function runCode()
@@ -19,5 +29,7 @@ function runCode()
     output.contentDocument.close();
 }
 
+codeArea.addEventListener("scroll", UpdateLines);
+codeArea.addEventListener("input", UpdateLines);
 window.addEventListener("load", Initialize);
 run.addEventListener("click", runCode);
